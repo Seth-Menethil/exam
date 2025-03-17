4badfc2425b94dcb3cac669616673fa92921f5c3
ca venti <venticancarlanthony@gmail.com>
2025-03-17 18:05:31 +0800

    Edit: Variable Names

--- Ventican_Carl.py
+++ Ventican_Carl.py
@@ -1,6 +1,6 @@
 print("=== Loan Payment Calculator ===\n1. Compute Monthly Payment\n2. Exit")
 choice=int(input("Choose an option (1-2): "))
 while choice == 1:
-    P=str(input("Enter loan principal: "))
-    r=str(input("Enter annual interest rate (in %): "))
-    n=int(input("Enter loan duration in years: "))
+    loanPrincipal=float(input("Enter loan principal: "))
+    interestRate=float(input("Enter annual interest rate (in %): "))
+    loandurationYears=float(input("Enter loan duration in years: "))
