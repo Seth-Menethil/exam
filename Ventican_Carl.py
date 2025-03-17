c91d65eda5a4d357ad48f08660f9231cc6be08ec
ca venti <venticancarlanthony@gmail.com>
2025-03-17 17:30:16 +0800

    Loan Principal

---
+++ Ventican_Carl.py
@@ -0,0 +1,4 @@
+print("=== Loan Payment Calculator ===\n1. Compute Monthly Payment\n2. Exit")
+choice=int(input("Choose an option (1-2): "))
+while choice == 1:
+    P=str(input("Enter loan principal: "))
